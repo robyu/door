197374a96c8d3f8ca6749276f2401bbd82ac32f6
robert yu <rob@signalessence.com>
2022-06-15 21:51:54 -0700

    changed mosquitto broker logging parameters

--- mqtt-test-broker/start-docker-mqtt.sh
+++ mqtt-test-broker/start-docker-mqtt.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+x=`docker ps | grep mqttserver`
+if [[ ! -z "$x" ]]; then
+   echo "stop existing mqttserver"
+   docker stop mqttserver
+fi
+   
 #
 # --rm remove container after exit
-docker run --name mqttserver --rm -it -p 1883:1883 -p 9001:9001 -v mosquitto.conf:`pwd`/mosquitto.conf   eclipse-mosquitto:2.0.14 mosquitto -c /mosquitto-no-auth.conf
+#
+# let mosquitto log to stdout, and have docker limit the log size & rotations
+# 
+echo "running mqttserver"
+docker run --name mqttserver --rm -p 1883:1883 -p 9001:9001 \
+       -v `pwd`/mosquitto.conf:/mosquitto/conf/mosquitto.conf \
+       --log-opt max-size=1m \
+       --log-opt max-file=3 \
+        eclipse-mosquitto:2.0.14 mosquitto -c /mosquitto/conf/mosquitto.conf
+
